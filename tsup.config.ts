dcc120ce36bc08175180a94d04bba995938d5d72
Hariom Kashyap <omkashyap6870@gmail.com>
2026-03-19 13:55:38 +0530

    setups

---
+++ tsup.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig, Options } from "tsup";
+import { minifyTemplates, writeFiles } from "esbuild-minify-templates";
+
+const commonConfig: Options = {
+  minify: true,
+  dts: true,
+  format: ["esm", "cjs"],
+  sourcemap: true,
+  clean: true,
+  esbuildPlugins: [minifyTemplates({ taggedOnly: false }), writeFiles()],
+};
+
+export default defineConfig([
+  {
+    ...commonConfig,
+    esbuildOptions: (options) => {
+      options.banner = {
+        js: "use client",
+      };
+    },
+    entry: ["src/index.ts"],
+    outDir: "dist",
+  },
+]);

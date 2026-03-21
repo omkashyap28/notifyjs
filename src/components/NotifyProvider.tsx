1fda4f13b3a1282b19eb27b9f412a1b3fbc895f6
Hariom Kashyap <omkashyap6870@gmail.com>
2026-03-21 20:57:03 +0530

    update default export to named export

--- src/components/NotifyProvider.tsx
+++ src/components/NotifyProvider.tsx
@@ -2,11 +2,9 @@ import { ReactNode } from "react";
 import { Provider } from "../core/store.js";
 import { NotifyProviderTypes } from "../core/types.js";
 
-const NotifyProvider = ({
+export const NotifyProvider = ({
   children,
   ...config
 }: NotifyProviderTypes & { children: ReactNode }) => {
   return <Provider config={config}>{children}</Provider>;
 };
-
-export default NotifyProvider;

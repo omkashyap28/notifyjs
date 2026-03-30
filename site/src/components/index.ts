1bb4f5472657e0e0fe778f119df4fb515a31045a
Hariom Kashyap <omkashyap6870@gmail.com>
2026-03-30 23:01:55 +0530

    export components

--- site/src/components/index.ts
+++ site/src/components/index.ts
@@ -1,6 +1,7 @@
 import Navbar from "./navbar";
+import Footer from "./footer";
 import SearchBar from "./search-bar";
 import Wrapper from "./wrapper";
 import Theme from "./theme";
 
-export { Navbar, SearchBar, Wrapper, Theme };
+export { Navbar, SearchBar, Wrapper, Theme, Footer };

08c8350626c8ac07519b10b3e22e1afe097bd23f
Hariom Kashyap <omkashyap6870@gmail.com>
2026-04-07 18:20:43 +0530

    export components from index file

--- site/src/components/docs/index.ts
+++ site/src/components/docs/index.ts
@@ -1,4 +1,9 @@
 import MainHeading from "./main-heading";
 import Sidebar from "./sidebar";
+import MdxCode from "./mdx-code";
+import MarginBlock from "./margin-block";
+import MdxContainer from "./mdx-container";
+import MdxFooter from "./mdx-footer";
 
-export { MainHeading, Sidebar };
+
+export { MainHeading, Sidebar, MdxCode, MarginBlock, MdxContainer, MdxFooter };

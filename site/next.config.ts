6e7dfbae0342632510684126106224daf9f550e9
Hariom Kashyap <omkashyap6870@gmail.com>
2026-04-07 18:13:59 +0530

    mdx configurations

--- site/next.config.ts
+++ site/next.config.ts
@@ -1,10 +1,16 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
+
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ["ts", "tsx", "mdx", "md"],
   turbopack: {
     root: ".",
   },
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);

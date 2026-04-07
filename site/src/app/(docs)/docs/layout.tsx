c2999a0aeaf60fe195203cbfa03306629391b300
Hariom Kashyap <omkashyap6870@gmail.com>
2026-04-07 18:20:14 +0530

    css for mdx

--- site/src/app/(docs)/docs/layout.tsx
+++ site/src/app/(docs)/docs/layout.tsx
@@ -1,14 +1,13 @@
-import { Sidebar } from "@/components/docs";
+import { MdxContainer, Sidebar } from "@/components/docs";
+import "./markdown.css";
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex max-md:flex-col w-full font-sans">
-        <Sidebar />
-      <main className="flex-1 p-4 md:px-18 md:py-8">{children}</main>
-
+    <div className="flex w-full font-sans max-md:flex-col">
+      <Sidebar />
+      <MdxContainer>{children}</MdxContainer>
       {/* area for adds */}
-      {/* <div className="sticky top-16 right-auto bottom-0 left-0 h-full w-66 overflow-y-auto p-4">
-      </div> */}
+      <div className="sticky top-16 right-auto bottom-0 left-0 h-full w-66 overflow-y-auto p-4"></div>
     </div>
   );
 }

4d40eb443d69886eca9b0b4e18dd4f4957014682
Hariom Kashyap <omkashyap6870@gmail.com>
2026-04-10 22:25:02 +0530

    removing add area

--- site/src/app/(docs)/docs/layout.tsx
+++ site/src/app/(docs)/docs/layout.tsx
@@ -1,4 +1,4 @@
-import { MdxContainer, Sidebar } from "@/components/docs";
+import { ContentsTable, MdxContainer, Sidebar } from "@/components/docs";
 import "./markdown.css";
 
 function Layout({ children }: { children: React.ReactNode }) {
@@ -6,8 +6,6 @@ function Layout({ children }: { children: React.ReactNode }) {
     <div className="flex w-full font-sans max-md:flex-col">
       <Sidebar />
       <MdxContainer>{children}</MdxContainer>
-      {/* area for adds */}
-      <div className="sticky top-16 right-auto bottom-0 left-0 h-full w-66 overflow-y-auto p-4"></div>
     </div>
   );
 }

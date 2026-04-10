f215a23a1b305a3f87257d9c173409d2187f17ff
Hariom Kashyap <omkashyap6870@gmail.com>
2026-04-10 23:39:21 +0530

    also append hooks link in this mdx-footer

--- site/src/components/docs/mdx-footer.tsx
+++ site/src/components/docs/mdx-footer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { componentsLinks, docsLinks } from "@/constants/links";
+import { componentsLinks, docsLinks, hooksLink } from "@/constants/links";
 import { ChevronRightIcon } from "@/icons";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -45,4 +45,4 @@ export default function MdxFooter() {
   );
 }
 
-const allLinks = [...docsLinks, ...componentsLinks];
+const allLinks = [...docsLinks, ...componentsLinks, ...hooksLink];
